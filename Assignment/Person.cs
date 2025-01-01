9081f6551f69cdc79dd674d87c8b8bcdcaee1e29
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-01 18:30:24 +0200

    Q 1

---
+++ Assignment/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal struct Person
+    {
+        public int Age;
+        public string Name;
+
+
+        public Person(int age, string name)
+        {
+            Age = age;
+            Name = name;
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        #region Part 01
+        #region Q 1
+        //static void Main(string[] args)
+        //{
+        //    Person[] arrayOfPerson = new Person[4];
+        //    arrayOfPerson[0] = new Person(21, "Mostafa");
+        //    arrayOfPerson[1] = new Person(22, "Ahmed");
+        //    arrayOfPerson[2] = new Person(23, "Ali");
+        //    arrayOfPerson[3] = new Person(24, "Mohamed");
+
+        //   foreach(var Person  in arrayOfPerson)
+        //    {
+        //        Console.WriteLine($" Name = {Person.Name} , Age = {Person.Age}");
+        //    }
+        //}
+        #endregion
+       
+        #endregion
+
     }
 }
