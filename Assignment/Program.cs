c33056049b582fc23f55af281d5a0abf6e57b7ff
mohammedelgarhy95@gmail.com <mohammedelgarhy95@gmail.com>
2025-01-01 19:36:35 +0200

    Q 2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace Assignment
+﻿using System.Drawing;
+using System.Reflection;
+
+namespace Assignment
 {
     internal class Program
     {
@@ -18,7 +21,44 @@
         //    }
         //}
         #endregion
-       
+        #region Q 2
+
+        //static double CalculateDistance(Point p1, Point p2)
+        //{
+
+        //    double dx = p2.x - p1.x;
+        //    double dy = p2.y - p1.y;
+        //    return Math.Sqrt(dx * dx + dy * dy);
+        //    // this is the library of math to solve the problem and the law is searched for it.
+
+        //    Console.WriteLine($"the distance betwwen 2 numbers is {dx},{dy}");
+
+
+
+        //    Point p1 = new Point(dx, dy);
+        //    Console.WriteLine("Enter the numbers of first point : ");
+        //    Console.WriteLine("X1 is :");
+        //    p1.x = Convert.ToDouble(Console.ReadLine());
+
+        //    Console.WriteLine("Y1 is :");
+        //    p1.y = Convert.ToDouble(Console.ReadLine());
+
+
+
+        //    Point p2 = new Point(dx, dy);
+        //    Console.WriteLine("Enter the numbers of first point : ");
+        //    Console.WriteLine("X2 is :");
+        //    p2.x = Convert.ToDouble(Console.ReadLine());
+
+        //    Console.WriteLine("Y2 is :");
+        //    p2.y = Convert.ToDouble(Console.ReadLine());
+
+
+
+           
+
+        //}
+        #endregion
         #endregion
 
     }
